20d5d220e341830ea42611e120063cc370d885a7
Seb <sebastiangbate@gmail.com>
2025-10-01 23:42:54 +0100

    fixing the urls

--- R/get_all_results.R
+++ R/get_all_results.R
@@ -4,28 +4,28 @@ library(rvest)
 library(xml2)
 library(stringr)
 
-
-get_results=function(event, eventno, headers=c(`User-Agent` = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36",
+get_results=function(url, headers=c(`User-Agent` = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36",
                                                `Accept` = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
                                                `Accept-Language` = "en-US,en;q=0.9",
                                                `Connection` = "keep-alive")){
   
-  response=GET(paste0("https://www.parkrun.org.uk/",str_to_lower(str_remove_all(event, "\\s")),"/results/",eventno,"/"),
+  response=GET(url,
                add_headers(.headers = headers),
                timeout(15))
   tryCatch({
+
     if(status_code(response)!=200){
-      stop(sprintf("Request failed [%d] for %s", code, url))
+      
+      message(sprintf("Request failed [%d] for %s", code, url))
     }
     html <- content(response, as = "text", encoding = "UTF-8") %>% read_html()
     tables <- html %>% html_element("div.Results.Results")
-    results=tables%>% html_table()
-    
+  
+    results <- tables %>% html_table() %>% dplyr::select(c(1, 2, 6))
+
+    names(results)=c("pos", "parkrunner", "time")
     results=results %>% mutate(parkrunner=str_extract(parkrunner, "^[^0-9]*") %>% str_trim(),
-                               time=str_extract(Time, "^[0-9:]+")) %>% 
-      dplyr::select("pos"=Position,
-                    parkrunner,
-                    time) %>% 
+                               time=str_extract(time, "^[0-9:]+")) %>% 
       drop_na(time) 
     
     return(results)
@@ -40,27 +40,50 @@ get_results=function(event, eventno, headers=c(`User-Agent` = "Mozilla/5.0 (Wind
   )
 }
 
-
-get_all_results=function(parkrunner,folder="data/results/",...){
+get_all_results=function(parkrunner,folder="data/results/",log_file="error_log.txt",...){
   if(class(parkrunner)!="parkrun_results") break
   results=parkrunner[["results"]]
   for(i in 1:nrow(results)){
     event=results$event[i]
     eventno=results$run_number[i]
-    
+    url=results$url[i]
+    errors=read.csv(log_file, header = T)
+    if(event %in% errors$event) next
     file=paste0(folder,event,eventno, ".csv")
     
     if(file.exists(file)) next
     
     cat(paste(event,eventno, "\n"))
+    tryCatch({
+      x=get_results(url=url)
+      
+      write.csv(x, file, row.names = F)
+      Sys.sleep(25)
+    },
+    error=function(e){
+      message("❌ Error: ", conditionMessage(e))
+      write(paste(event, eventno, sep = ","), file = log_file, append = TRUE)
+      
+    },
+    warning=function(e){
+      message(conditionMessage(e))
+    }
+    )
     
-    x=get_results(event=event, eventno = eventno)
-    
-    write.csv(x, file)
-    Sys.sleep(25)
   }
   return(0)
 }
 
+
 load("data/all_parkruns.RDa")
-get_all_results(all_parkruns[["sebastianbate"]])
+
+for(j in names(all_parkruns)){
+  cat(crayon::blue(paste(j, "\n")))
+  get_all_results(all_parkruns[[j]])
+}
+# ls=list.files("data/results", full.names = T)
+# for(i in ls){
+#   read.csv(i) %>% 
+#     dplyr::select(pos,parkrunner,time) %>% 
+#     write.csv(i, row.names = F)
+# }
--- R/get_all_runs.R
+++ R/get_all_runs.R
@@ -20,7 +20,26 @@ get_all_runs=function(id, headers=c(`User-Agent` = "Mozilla/5.0 (Windows NT 10.0
     html <- content(response, as = "text", encoding = "UTF-8") %>% read_html()
     tables <- html %>% html_elements("table#results")
     results=tables[[3]] %>% html_table()
+    href= tables[[3]] %>%
+      html_nodes("a") %>%
+      html_attr("href")
+
+    results$url<- href[seq(3, length(href)+2, by = 3)]
     
+    
+    
+    results=results %>% 
+      janitor::clean_names() %>% 
+      mutate(event=case_when(event=="Henley Wood Oswestry"~"Henley Wood",
+                             event=="Bramhall Park"~"Bramhall",
+                             event=="Finsbury Park"~"Finsbury",
+                             event=="Bushy Park"~"Bushy",
+                             event=="Heaton Park"~"Heaton",
+                             event=="Y Promenâd Abermaw"~"Y Promenad Abermaw",
+                             event=="Gorleston Cliffs"~"Gorleston",
+                             event=="Stratford-upon-Avon"~"Stratford Upon Avon",
+                             T~event) %>% 
+               str_replace_all("[[:punct:]]", ""))
     
     h2_nodes <- html %>% html_elements("h2")
     
@@ -32,7 +51,8 @@ get_all_runs=function(id, headers=c(`User-Agent` = "Mozilla/5.0 (Windows NT 10.0
     
     out=list(name=name,
              id=id,
-             results=results %>% janitor::clean_names())
+             results=results
+    )
     
     structure(out,
               class="parkrun_results")
@@ -48,42 +68,42 @@ get_all_runs=function(id, headers=c(`User-Agent` = "Mozilla/5.0 (Windows NT 10.0
 }
 
 ids=tribble(~id,
-'7232608', # Adam
-'4087050', # Alex
-'7402459', # Andy
-'7072913', # Nat
-'4458732', # Frankie
-'593256', # Jonny
-'2407768', # Luke
-'582473', # Max
-'7266141', # Phil
-'1539187', # Rachel
-'5349926', # Rob
-'493595', # Seb
-'7756087', # Suzy
-'5243446', # Tom A
-'2548951', #charlotte
-'5107378', #mike
-'7073694', #jgunz
-#bate
-'147910', #mum
-'144486', #ewan
-'140961', #dad
-
-#wnr
-
-"42804", #paul M
-"9334474", #Alex beresford
-"6391679", #kirsty
-"7433025", #Helen
-"4301378", #Lindsay
-"4051781", #Callum
-"16568", #Paul C
-"16569", #Lynda
-"3629365", #Isabel
-"888332", #Colm
-"1166640", #gary
-"81779", #jon
+            '7232608', # Adam
+            '4087050', # Alex
+            '7402459', # Andy
+            '7072913', # Nat
+            '4458732', # Frankie
+            '593256', # Jonny
+            '2407768', # Luke
+            '582473', # Max
+            '7266141', # Phil
+            '1539187', # Rachel
+            '5349926', # Rob
+            '493595', # Seb
+            '7756087', # Suzy
+            '5243446', # Tom A
+            '2548951', #charlotte
+            '5107378', #mike
+            '7073694', #jgunz
+            #bate
+            '147910', #mum
+            '144486', #ewan
+            '140961', #dad
+            
+            #wnr
+            
+            "42804", #paul M
+            "9334474", #Alex beresford
+            "6391679", #kirsty
+            "7433025", #Helen
+            "4301378", #Lindsay
+            "4051781", #Callum
+            "16568", #Paul C
+            "16569", #Lynda
+            "3629365", #Isabel
+            "888332", #Colm
+            "1166640", #gary
+            "81779", #jon
 )
 
 all_parkruns=list()
@@ -91,7 +111,12 @@ for(i in ids$id){
   hold=get_all_runs(i)
   cat(paste(hold$name, "\n"))
   all_parkruns[[str_to_lower(str_remove_all(hold$name, "\\s"))]]=hold
-  Sys.sleep(30)
+  Sys.sleep(25)
 }
 
 save(all_parkruns, file="data/all_parkruns.RDa")
+
+# for(i in names(all_parkruns)){
+#   all_parkruns[[i]][["results"]]=all_parkruns[[i]][["results"]] %>% 
+#     mutate(event=event %>% str_replace_all(",", ""))
+# }
